2bb254beedfea1a8fb249c3d95ca57688dd998ed
yash <yashgadle@gmail.com>
2019-09-22 18:00:41 +0530

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,44 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import img1 from './res/img1.jpg';
+import img2 from './res/img2.jpg';
+import img3 from './res/img3.jpg';
+import img4 from './res/img4.jpg';
+import img5 from './res/img5.jpg';
+import img6 from './res/img6.jpg';
+import img7 from './res/img7.jpg';
+
+import { Img } from './Img';
+
+import './App.scss';
+
+const images = [null, img7, img1, img2, img3, img4, img5, img6, null];
 
 const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+	return (
+		<div id="carousel" className="Carousel">
+			{images.map((image, index) =>
+				image ? (
+					<Img
+						key={image}
+						url={image}
+						width={600}
+						height={600}
+						index={index}
+					/>
+				) : (
+					<div
+						key={index}
+						style={{
+							minWidth: '20rem',
+							height: 400,
+							position: 'relative'
+						}}
+					></div>
+				)
+			)}
+		</div>
+	);
+};
 
 export default App;
---
+++ src/Img.tsx
@@ -0,0 +1,74 @@
+import React from 'react';
+
+import classnames from 'classnames';
+
+interface Props {
+	url: string;
+	index: number;
+	width: string | number;
+	height: string | number;
+}
+
+export const Img: React.FC<Props> = props => {
+	const [imageType, setImageType] = React.useState('landscape');
+	const { url, width, height, index } = props;
+
+	const addObserver = (img: HTMLImageElement) => {
+		const observer = new IntersectionObserver(
+			(entries, observer) => {
+				entries.forEach(entry => {
+					// console.log(entry);
+					const x = document.getElementById(
+						'image-container-' + index
+					);
+					if (entry.isIntersecting) {
+						img.style.opacity = '1';
+						if (imageType === 'landscape') x.style.width = '800px';
+						else x.style.height = '800px';
+					} else {
+						img.style.opacity = '0.5';
+						if (imageType === 'landscape') x.style.width = '600px';
+						else x.style.height = '600px';
+					}
+				});
+			},
+			{
+				root: document.querySelector('#carousel'),
+				rootMargin: '0px',
+				threshold: 0.9
+			}
+		);
+
+		observer.observe(img);
+	};
+
+	return (
+		<div
+			id={'image-container-' + index}
+			className={classnames({
+				'carousel-image-portrait': imageType === 'portrait',
+				'carousel-image-landscape': imageType === 'landscape'
+			})}
+			style={{
+				width: imageType === 'landscape' ? width : 'fit-content',
+				height: imageType === 'portrait' ? height : 'fit-content'
+			}}
+		>
+			<img
+				id={'img'}
+				src={url}
+				style={{ opacity: 0.5, transition: '0.5s opacity' }}
+				onLoad={e => {
+					const {
+						naturalHeight,
+						naturalWidth
+					} = e.target as HTMLImageElement;
+					if (naturalWidth < naturalHeight) setImageType('portrait');
+					else setImageType('landscape');
+
+					addObserver(e.target as HTMLImageElement);
+				}}
+			/>
+		</div>
+	);
+};
