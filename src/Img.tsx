03968a7e52f65fb938f56f817b10689e534a3cb0
yash <yashgadle@gmail.com>
2019-09-22 18:58:51 +0530

    Played around with some style values through javascript

--- src/Img.tsx
+++ src/Img.tsx
@@ -23,21 +23,27 @@ export const Img: React.FC<Props> = props => {
 					);
 					if (entry.isIntersecting) {
 						img.style.opacity = '1';
-						if (img.naturalWidth > img.naturalHeight)
-							x.style.width = '800px';
-						else x.style.height = '650px';
+						// if (img.naturalWidth > img.naturalHeight)
+						// 	x.style.width = '800px';
+						// else x.style.height = '650px';
+						if (img.naturalWidth < img.naturalHeight)
+							x.style.transform = 'scale(1)';
+						else x.style.transform = 'scale(1.2)';
 					} else {
 						img.style.opacity = '0.5';
-						if (img.naturalWidth > img.naturalHeight)
-							x.style.width = '600px';
-						else x.style.height = '600px';
+						// if (img.naturalWidth > img.naturalHeight)
+						// 	x.style.width = '600px';
+						// else x.style.height = '600px';
+						if (img.naturalWidth < img.naturalHeight)
+							x.style.transform = 'scale(0.8)';
+						else x.style.transform = 'scale(0.9)';
 					}
 				});
 			},
 			{
 				root: document.querySelector('#carousel'),
 				rootMargin: '0px',
-				threshold: 0.99
+				threshold: 0.9
 			}
 		);
 
